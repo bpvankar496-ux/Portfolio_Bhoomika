3246fe60e01d224d906c94e30745666cff2f12e5
Bhoomika Vankar <bpvankar496@gmail.com>
2026-08-01 12:31:19 +0530

    Updated portfolio

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 require('dotenv').config();
 
 const express = require('express');
+
 const path = require('path');
 const nodemailer = require('nodemailer');
 
 const app = express();
+app.use(express.static("public"));
 const PORT = process.env.PORT || 3000;
 
 app.set('view engine', 'ejs');
